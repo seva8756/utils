f5d43a937693b6f2d01dcca31926a1981a932fbb
seva8756 <seva8756@gmail.com>
2023-02-24 13:49:17 +0300

    ref: function Contains renamed to InSlice

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/seva8756/utils
+module github.com/seva8756/utils/v2
 
 go 1.19
--- utils.go
+++ utils.go
@@ -1,6 +1,6 @@
 package utils
 
-func Contains(a []string, x string) bool {
+func InSlice(a []string, x string) bool {
 	for _, v := range a {
 		if x == v {
 			return true
